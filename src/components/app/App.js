1d99c12e59a41d802bc7861205dafc29f4e9c332
Gedas11 <97019328+Gedas11@users.noreply.github.com>
2022-03-08 22:22:28 +0100

    2022-03-08
    namu darbas

---
+++ src/components/app/App.js
@@ -0,0 +1,44 @@
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import MovieList from './MovieList';
+import MovieListHeading from "../MovieListHeading";
+import SearchBox from "../SearchBox";
+
+const App = () => {
+  const [movies, setMovies] = useState([])
+  const [searchValue, setSearchValue] = useState('');
+
+    const getMovieRequest = async () => {
+      const url = "https://www.omdbapi.com/?s=terminator&apikey=263d22d8"
+
+      const response = await fetch(url);
+      const responseJson = await response.json();
+
+      console.log(responseJson);
+      setMovies(responseJson.Search)
+
+  };
+
+useEffect (()=>{
+  getMovieRequest();
+}, [] );
+
+
+  return <div className="container-fluid movie-app">
+    <div className="row d-flex align-item-center mt-4 mb-4">
+        <MovieListHeading heading='Movies' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+    </div>
+    <div className="row">
+    <MovieList movies = {movies} />
+  </div>
+    
+  </div>
+}
+
+export default App;
+
+
